ce8b05c2b10b984e4540b26b4f0da1f62459ff60
Omar Rodriguez <omar.rguez26@gmail.com>
2022-10-01 18:56:29 -0400

    added missing links to Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-function Footer () {
-    return(
+function Footer() {
+    return (
         <footer>
             <h2>Made with ❤️ by Nickless192</h2>
             <p>
                 &copy; 2022 Nickless192
             </p>
+            <div>
+                <a href="https://github.com/nickless192" target="_blank" rel="noreferrer">Link to Github Account</a>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/omar-rodriguez26/" target="_blank" rel="noreferrer">Let's connect on LinkedIn!</a>
+            </div>
+            <div>
+                <a href="https://stackoverflow.com/users/18967473/omar-rodriguez" target="_blank" rel="noreferrer">Find me on StackOverflow</a>
+            </div>
         </footer>
     )
 }
